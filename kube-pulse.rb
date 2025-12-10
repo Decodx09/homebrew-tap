c899b4878b94a2bbb8eb40ee584d7b9cc0ee2697
shivansh <53109024+Decodx09@users.noreply.github.com>
2025-12-10 10:10:10 +0530

    just take it

--- kube-pulse.rb
+++ kube-pulse.rb
@@ -2,7 +2,7 @@ class KubePulse < Formula
   desc "A Real-Time K8s Dashboard CLI"
   homepage "https://github.com/Decodx09/kube-pulse"
   
-  url "https://github.com/Decodx09/kube-pulse/releases/download/v1.0.0/kube-pulse-v1.0.0.tar.gz"
+  url "https://github.com/Decodx09/kube-pulse/releases/download/v1.1.1/kube-pulse-v1.1.1.tar.gz"
   
   sha256 "b006929d775c52b6185d9ed58fcb8eb35d21049f51c0e47f329d0a6774e3a754"
   
